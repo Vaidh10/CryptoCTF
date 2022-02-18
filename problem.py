2ce2b2216e87bfe020428a8d66c04872f6aef652
VR <79251803+Vaidh10@users.noreply.github.com>
2022-02-18 16:08:14 -0500

    Update problem.py

--- problem.py
+++ problem.py
@@ -1,6 +1,5 @@
 from Crypto.Util.number import getPrime, inverse, bytes_to_long, long_to_bytes
 
-
 p = getPrime(2048)
 q = getPrime(2048)
 e = 3
@@ -12,7 +11,6 @@ n = p * q
 flag = b"ctf{XXXXXXXXXXXXXXXXXXX}"
 pt = bytes_to_long(flag)
 ct = pow(pt, e, n)
-
 
 print(f"n = {n}")
 print(f"e = {e}")
